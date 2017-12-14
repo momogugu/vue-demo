c6ad6ac73e734908c9d00b6f308cf72351ae37a4
momogugu <momogugu1107@gmail.com>
2017-12-14 22:54:47 +0800

    modified action

--- vue-express/src/vuex/actions.js
+++ vue-express/src/vuex/actions.js
@@ -6,19 +6,16 @@ const actions = {
     state
   }, obj) {
     return new Promise((resolve, reject) => {
-      axios.get('http://127.0.0.1:3000/cart/123').then(response => {
-        console.log(response)
-      }).catch(response => {})
-      //   axios.post('/api/registe', {
-      //     username: obj.username,
-      //     nickname: obj.nickname,
-      //     password: obj.password,
-      //     description: obj.description
-      //   }).then(response => {
+      axios.post('/api/registe', {
+        username: obj.username,
+        nickname: obj.nickname,
+        password: obj.password,
+        description: obj.description
+      }).then(response => {
 
-      //   }).catch(response => {
+      }).catch(response => {
 
-      //   })
+      })
     })
   }
 }
