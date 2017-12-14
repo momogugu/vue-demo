04770f425c4add92a323909fc6178eafdb1f1acd
momogugu <momogugu1107@gmail.com>
2017-12-14 23:09:15 +0800

    modified express

--- vue-express/server/routes/route.js
+++ vue-express/server/routes/route.js
@@ -1,5 +1,5 @@
 export default (app) => {
-  app.get('/cart/:username', function (req, res) {
+  app.get('/api/cart/:username', function (req, res) {
     res.send('Hello World!');
   });
   app.post('/api/registe', function (req, res) {
